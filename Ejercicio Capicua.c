641f7f8072eaf1fb57f63115a763ddf1747bf878
Alexlehander <47540077+alexlehander@users.noreply.github.com>
2019-04-27 20:53:03 -0700

    Update Ejercicio Capicua.c

--- Ejercicio Capicua.c
+++ Ejercicio Capicua.c
@@ -2,17 +2,17 @@
 int main () {
 	int tamano,opcion,i;
 	int contador2=0, eterno=0;
-	printf ("\nDame el tamanio de tu numero \n");
+	printf ("\nDame el tamanio de su numero \n");
 	scanf ("%d", &tamano);
 	int numero[tamano], pila[tamano], cola[tamano];
 	int contador1=tamano-1;
 	system("cls");
 	while (eterno==0) {
-		printf ("\nDame la opcion: \n1-Insertar \n2-Mostrar \n3-Salir \n");
+		printf ("\nDame la opcion: \n1-Anotar el numero \n2-Comparar si es Palindrome \n3-Salir \n");
 		scanf ("%d", &opcion);
 		system("cls");
 		if (opcion==1) {
-    	    printf ("\nIntroduze el numero a insertar \n");
+    	    printf ("\nIntroduzca el numero deseado \n");
     	    for (i=0;i<tamano;i++) {
     	    	scanf ("%d", &numero[i]);
     	    	pila[contador1]=numero[i];
@@ -26,7 +26,7 @@ int main () {
 		        if (cola[i]==pila[i]) tamano--;
 		        printf ("\n%d %d", cola[i], pila[i]);
 	        }
-	        if (tamano==0) printf ("\nEs capicua");
+	        if (tamano==0) printf ("\nEs Palindrome");
 	        else printf ("\nNo lo es");
 		}
         if (opcion==3) {
